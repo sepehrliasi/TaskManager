efc9137fa13b1a98dbde08649225a2582e56f5b2
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2023-07-06 09:16:50 +0330

    Toggle Status added

---
+++ server/database/ToggleStatus.php
@@ -0,0 +1,32 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/TaskManager/server/database/confdb.php";
+
+$db= $conn;
+$taskid = $_GET['id'];
+
+$fetchData = fetch_data($db, $taskid);
+
+function fetch_data($db, $taskid){
+ if(empty($db)){
+  $msg= "Database connection error";
+ }elseif(empty("tasks")){
+   $msg= "Table Name is empty";
+ }else{
+
+    $query = "SELECT * FROM tasks WHERE taskid = '$taskid'";
+    $result = $db->query($query);
+
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row['isComplete'] == true){
+        $sql = "UPDATE tasks SET isComplete=false WHERE taskid='$taskid'";
+    }else{
+        $sql = "UPDATE tasks SET isComplete=true WHERE taskid='$taskid'";
+    }
+    $result = $db->query($sql);
+    $msg = $row;
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+ }
+ return $msg;
+}
+?>
