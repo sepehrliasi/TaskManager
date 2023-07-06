538fe69259d8fa406bd2bc6a345bc24ecaadb921
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2023-07-06 16:00:37 +0330

    delete task updated

--- server/database/DeleteTask.php
+++ server/database/DeleteTask.php
@@ -7,6 +7,13 @@ $taskid = $_GET['id'];
 
     $sql = "DELETE from tasks WHERE taskid='$taskid'";
     mysqli_query($conn, $sql);
+
+    $sql2 = "DELETE from userTasks WHERE taskid='$taskid'";
+    mysqli_query($conn, $sql2);
+
+    $sql3 = "DELETE from taskdependencies WHERE childTask='$taskid' or parentTask='$taskid'";
+    mysqli_query($conn, $sql3);
+
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 mysqli_close($conn);
